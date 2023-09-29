c5781a3752c4a89a81de64a70d8465f4569c1716
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2023-09-29 21:52:21 +0100

    added drag and drop and refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <cstdio>
 #include <iostream>
 #include <raylib.h>
 
@@ -7,99 +8,59 @@
 #define WINDOW_TITLE "mvisual"
 #define MAX_FPS 60
 
-bool isPlaying = true;
-bool isFPSShown = false;
-
 enum class MODE
 {
-    SIMPLE_MODE,
-    VISUALATION_MODE,
+    MUSIC_UNLOADED,
+    SIMPLE,
+    VISUALATION,
 };
 
-// Ported from https://rosettacode.org/wiki/Fast_Fourier_transform#Python
+auto calculateTextCenterPosX = [](const char* text) { return (WINDOW_WIDTH - MeasureText(text, 20)) / 2; };
+
+Music music;
+bool isFPSShown = false;
+auto currentMod = MODE::MUSIC_UNLOADED;
+
+void drawSimpleMode();
+void drawVisualationMode();
+void handleInput();
 
 int main(void)
 {
-    auto currentMod = MODE::SIMPLE_MODE;
     InitWindow(WINDOW_WIDTH, WINDOW_HIGHT, WINDOW_TITLE);
     InitAudioDevice();
-
-    auto music = LoadMusicStream("song.mp3");
-    PlayMusicStream(music);
     SetTargetFPS(MAX_FPS);
 
     while (!WindowShouldClose())
     {
         UpdateMusicStream(music);
 
-        auto timePlayed = GetMusicTimePlayed(music) / GetMusicTimeLength(music);
-        if (timePlayed > 1.0f) timePlayed = 1.0;
-
-        if (IsKeyPressed(KEY_P))
-        {
-            (isPlaying) ? PauseMusicStream(music) : ResumeMusicStream(music);
-            isPlaying = !isPlaying;
-        }
-        if (IsKeyPressed(KEY_SPACE))
-        {
-            StopMusicStream(music);
-            PauseMusicStream(music);
-        }
-        if (IsKeyPressed(KEY_F))
-        {
-            isFPSShown = !isFPSShown;
-        }
-        if (IsKeyPressed(KEY_V))
-        {
-            switch (currentMod)
-            {
-            case MODE::SIMPLE_MODE:
-                currentMod = MODE::VISUALATION_MODE;
-                break;
-            case MODE::VISUALATION_MODE:
-                currentMod = MODE::SIMPLE_MODE;
-                break;
-            }
-        }
-
-        auto calculateTextCenterPosX = [](const char* text) { return (WINDOW_WIDTH - MeasureText(text, 20)) / 2; };
+        handleInput();
 
         BeginDrawing();
 
-        if(isFPSShown) {
+        if (isFPSShown)
+        {
             DrawFPS(0, 0);
         }
 
-        if (currentMod == MODE::SIMPLE_MODE)
+        if (currentMod == MODE::SIMPLE)
         {
-            ClearBackground(RAYWHITE);
-
-            DrawText("MUSIC SHOULD BE PLAYING!", 255, 100, 20, LIGHTGRAY);
-
-            DrawRectangle(200, 150, 400, 12, LIGHTGRAY);
-            DrawRectangle(200, 150, (int)(timePlayed * 400.0f), 12, MAROON);
-            DrawRectangleLines(200, 150, 400, 12, GRAY);
-
-            auto restartText = "PRESS SPACE TO RESTART MUSIC";
-            auto pauseText = "PRESS P TO PAUSE/RESUME MUSIC";
-            auto visualationText = "PRESS V TO ENTER VISUALATION MODE";
-            auto fpsText = "PRESS F TO TOGGEL FPS";
-
-            DrawText(restartText, calculateTextCenterPosX(restartText), 200, 20, LIGHTGRAY);
-            DrawText(pauseText, calculateTextCenterPosX(pauseText), 230, 20, LIGHTGRAY);
-            DrawText(visualationText, calculateTextCenterPosX(visualationText), 260, 20, LIGHTGRAY);
-            DrawText(fpsText, calculateTextCenterPosX(fpsText), 290, 20, LIGHTGRAY);
-
+            drawSimpleMode();
             EndDrawing();
             continue;
         }
-
-        if (currentMod == MODE::VISUALATION_MODE)
+        if (currentMod == MODE::VISUALATION)
+        {
+            drawVisualationMode();
+            EndDrawing();
+            continue;
+        }
+        if (currentMod == MODE::MUSIC_UNLOADED)
         {
             ClearBackground(RAYWHITE);
-            auto text = "WELCOME TO VISUALATION MODE STILL IN DEVELEPMENT";
+            auto text = "WELCOME TO MVISAL PLEASE DRAG AND DROP YOUR MUSIC";
             DrawText(text, calculateTextCenterPosX(text), 20, 20, LIGHTGRAY);
-
             EndDrawing();
             continue;
         }
@@ -110,4 +71,85 @@ int main(void)
     CloseWindow();
 
     return 0;
+}
+
+void drawSimpleMode()
+{
+
+    float timePlayed = GetMusicTimePlayed(music) / GetMusicTimeLength(music);
+    if (timePlayed > 1.0f) timePlayed = 1.0;
+
+    ClearBackground(RAYWHITE);
+
+    DrawText("MUSIC SHOULD BE PLAYING!", 255, 100, 20, LIGHTGRAY);
+
+    DrawRectangle(200, 150, 400, 12, LIGHTGRAY);
+    DrawRectangle(200, 150, (int)(timePlayed * 400.0f), 12, MAROON);
+    DrawRectangleLines(200, 150, 400, 12, GRAY);
+
+    auto restartText = "PRESS SPACE TO RESTART MUSIC";
+    auto pauseText = "PRESS P TO PAUSE/RESUME MUSIC";
+    auto visualationText = "PRESS V TO ENTER VISUALATION MODE";
+    auto fpsText = "PRESS F TO TOGGEL FPS";
+
+    DrawText(restartText, calculateTextCenterPosX(restartText), 200, 20, LIGHTGRAY);
+    DrawText(pauseText, calculateTextCenterPosX(pauseText), 230, 20, LIGHTGRAY);
+    DrawText(visualationText, calculateTextCenterPosX(visualationText), 260, 20, LIGHTGRAY);
+    DrawText(fpsText, calculateTextCenterPosX(fpsText), 290, 20, LIGHTGRAY);
+}
+
+void drawVisualationMode()
+{
+    ClearBackground(RAYWHITE);
+    auto text = "WELCOME TO VISUALATION MODE STILL IN DEVELEPMENT";
+    DrawText(text, calculateTextCenterPosX(text), 20, 20, LIGHTGRAY);
+}
+
+void handleInput()
+{
+
+    if (IsFileDropped())
+    {
+        UnloadMusicStream(music);
+
+        FilePathList files = LoadDroppedFiles();
+
+        music = LoadMusicStream(files.paths[0]);
+        PlayMusicStream(music);
+
+        if (IsMusicStreamPlaying(music) && currentMod == MODE::MUSIC_UNLOADED) currentMod = MODE::SIMPLE;
+
+        UnloadDroppedFiles(files);
+    }
+
+    if (IsKeyPressed(KEY_P) && currentMod != MODE::MUSIC_UNLOADED)
+    {
+        (IsMusicStreamPlaying(music)) ? PauseMusicStream(music) : ResumeMusicStream(music);
+    }
+    
+    if (IsKeyPressed(KEY_SPACE) && currentMod != MODE::MUSIC_UNLOADED)
+    {
+        StopMusicStream(music);
+        PlayMusicStream(music);
+    }
+    
+    if (IsKeyPressed(KEY_F))
+    {
+        isFPSShown = !isFPSShown;
+    }
+
+    if (IsKeyPressed(KEY_V) && currentMod != MODE::MUSIC_UNLOADED)
+    {
+        switch (currentMod)
+        {
+        case MODE::SIMPLE:
+            currentMod = MODE::VISUALATION;
+            break;
+        case MODE::VISUALATION:
+            currentMod = MODE::SIMPLE;
+            break;
+        case MODE::MUSIC_UNLOADED:
+            break;
+        }
+    }
 }
